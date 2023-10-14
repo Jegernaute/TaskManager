727ee1c82ad4a5b733b92e536d44188ffe6ac933
Bogdan Frolov <frolovbogdan01@gmail.com>
2023-10-14 14:34:23 +0300

    Перший коміт: Додав стартовий Django проект та моделі

---
+++ tasks/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Category(models.Model):
+    name_of_category = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name_of_category
+
+class Task(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField(auto_now_add=True)
+    priority = models.IntegerField()
+    status_of_tasks = models.CharField(max_length=255)
+    date_of_create = models.DateTimeField(auto_now_add=True)
+
+
